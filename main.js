d6c9520e8218862cbe13adb0ff39c23ebe8cac27
MIN <65863834+kxxrxmxn@users.noreply.github.com>
2021-06-22 23:43:33 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ function displayResults (weather){
 
 }
 
+
 function dateBuilder (d){
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -49,3 +50,49 @@ function dateBuilder (d){
 
     return `${day} ${date} ${month} ${year}`;
 }
+
+
+// current location
+
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api.key}&units=metric`)
+    .then(function(response){
+      return response.json();
+   })
+   .then(function(json){
+       const place = json.name;
+       getResults(place);
+   })
+}
+
+const COORDS = 'coords';
+   
+function getLocation(){
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+   }
+   
+
+function success(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+};
+
+function error(){
+    console.log('cant access current location');
+
+}
+
+getLocation();
+
+
+
+
